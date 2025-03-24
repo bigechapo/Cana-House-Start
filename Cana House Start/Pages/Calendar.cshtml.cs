889c28090f0355cd3c41d9ec8c023662d72dc2f1
Eric Rafael Chavez <lileroc123@gmail.com>
2025-03-24 10:49:47 -0500

    Updated all pages except Event A/B and gallery

--- Cana House Start/Pages/Calendar.cshtml.cs
+++ Cana House Start/Pages/Calendar.cshtml.cs
@@ -1,12 +1,30 @@
-using Microsoft.AspNetCore.Mvc;
+using CanaHouse.Data;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Cana_House_Start.Pages
 {
     public class CalendarModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _context;
+
+        public CalendarModel(ApplicationDbContext context)
         {
+            _context = context;
+        }
+
+        public List<Event> Event { get; set; } = new List<Event>();
+
+        public async Task OnGetAsync()
+        {
+            var today = DateTime.Now;
+            Event = await _context.Event
+                .Where(e => e.Date.Month == today.Month && e.Date.Year == today.Year)
+                .ToListAsync();
         }
     }
 }
